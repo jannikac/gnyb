058d8edebeb7f380d86cf1a74b3994b9313fee67
jannikac <jannikac@mailbox.org>
2024-11-04 17:58:54 +0100

    update navbar with submenu

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -2,6 +2,14 @@ import { IconMenu2 } from "@tabler/icons-react";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
 import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "~/components/ui/navigation-menu";
+import {
   Sheet,
   SheetClose,
   SheetContent,
@@ -15,19 +23,68 @@ interface NavbarItem {
   href: string;
 }
 
-const NavbarItems: NavbarItem[] = [
+interface NavbarSubMenu {
+  name: string;
+  children: NavbarItem[];
+}
+
+type NavbarData = Array<NavbarItem | NavbarSubMenu>;
+
+const isSubMenu = (arg: NavbarItem | NavbarSubMenu): arg is NavbarSubMenu => {
+  return "children" in arg && "name" in arg;
+};
+
+const NavbarItems: NavbarData = [
   { name: "Serverinfos", href: "/servers" },
-  { name: "Apex Generator", href: "/roll/apex" },
-  { name: "LoL Generator", href: "/roll/lol" },
   { name: "Wichteln", href: "/wichteln" },
+  {
+    name: "Randomizer",
+    children: [
+      { name: "Apex Randomizer", href: "/roll/apex" },
+      { name: "LoL Randomizer", href: "/roll/lol" },
+    ],
+  },
+  {
+    name: "Werkzeuge",
+    children: [{ name: "ICAL Konverter", href: "/tools/ical" }],
+  },
 ];
 
 const Navbar = () => {
   return (
     <nav className="flex text-nowrap">
-      {NavbarItems.map((item) => (
-        <NavBarItem key={item.name} name={item.name} href={item.href} />
-      ))}
+      <NavigationMenu className="hidden sm:flex">
+        <NavigationMenuList style={{ display: "flex", listStyle: "none" }}>
+          {NavbarItems.map((item) => (
+            <NavigationMenuItem key={item.name}>
+              {isSubMenu(item) ? (
+                <>
+                  <NavigationMenuTrigger>{item.name}</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul>
+                      {item.children.map((item) => (
+                        <li key={item.href}>
+                          <NavigationMenuLink
+                            className="w-full justify-start"
+                            key={item.href}
+                            href={item.href}
+                          >
+                            {item.name}
+                          </NavigationMenuLink>
+                        </li>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </>
+              ) : (
+                <NavigationMenuLink href={item.href}>
+                  {item.name}
+                </NavigationMenuLink>
+              )}
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
       <Sheet>
         <SheetTrigger asChild>
           <Button
@@ -42,34 +99,50 @@ const Navbar = () => {
           <SheetHeader>
             <SheetTitle>Navigation</SheetTitle>
           </SheetHeader>
-          <div className="flex flex-col">
-            <SheetClose asChild>
-              <Link className="p-2 hover:bg-muted/50" href={"/"}>
-                Home
-              </Link>
-            </SheetClose>
-            {NavbarItems.map((item) => (
-              <SheetClose key={item.name} asChild>
-                <Link className="p-2 hover:bg-muted/50" href={item.href}>
-                  {item.name}
-                </Link>
-              </SheetClose>
-            ))}
-          </div>
+          <nav>
+            <ul className="flex flex-col">
+              <li className="flex">
+                <SheetClose asChild>
+                  <Link className="w-full p-2 hover:bg-muted/50" href={"/"}>
+                    Home
+                  </Link>
+                </SheetClose>
+              </li>
+              {NavbarItems.map((item) => (
+                <li className="flex flex-col" key={item.name}>
+                  {isSubMenu(item) ? (
+                    <>
+                      <p className=" p-2">{item.name}</p>
+                      <ul className="flex flex-col gap-2">
+                        {item.children.map((item) => (
+                          <li className="flex" key={item.name}>
+                            <SheetClose asChild>
+                              <Link
+                                key={item.href}
+                                className="ml-4 w-full p-2 hover:bg-muted/50"
+                                href={item.href}
+                              >
+                                {item.name}
+                              </Link>
+                            </SheetClose>
+                          </li>
+                        ))}
+                      </ul>
+                    </>
+                  ) : (
+                    <SheetClose key={item.name} asChild>
+                      <Link className="p-2 hover:bg-muted/50" href={item.href}>
+                        {item.name}
+                      </Link>
+                    </SheetClose>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
         </SheetContent>
       </Sheet>
     </nav>
-  );
-};
-
-const NavBarItem = ({ name, href }: NavbarItem) => {
-  return (
-    <Link
-      className="hidden rounded-[--radius] p-4 text-lg hover:bg-muted/50 sm:inline"
-      href={href}
-    >
-      {name}
-    </Link>
   );
 };
 
